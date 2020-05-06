b2735f905a9df6002e20dcd670ee0779953445cb
Nicholas Devenish <ndevenish@gmail.com>
2020-05-06 15:31:16 +0100

    Quick commit of graph printing

--- src/ossify/grammar.py
+++ src/ossify/grammar.py
@@ -1,41 +1,73 @@
 from __future__ import annotations
 
-from typing import Any, List, NamedTuple, Union
+import tokenize
+import io
+from typing import Any, List, NamedTuple, Union, Iterable
 
 
 class ScopeName(NamedTuple):
     parts: List[str]
-    # def __init__(self, parts):
-    #     self.parts = parts
-    #     # print("Building ScopeName", parts)
-    #     # breakpoint()
-    # def __repr__(self):
-    #     return f"ScopeName({self.parts!r})"
-
+    
+    def __str__(self):
+        return ".".join(self.parts)
 
 ScopeOptions = dict
 DefinitionOptions = dict
 
+
+def _render_graph(stream, node, indent: str = "", last=True, first=True):
+    """Draw a textual representation of the node graph"""
+    if first:
+        first_i = ""
+        second_i = ""
+    elif last:
+        first_i = "╰─"
+        second_i = "  "
+    else:
+        first_i = "├─"
+        second_i = "│ "
+    node_name = str(node)
+    if isinstance(node, dict) or isinstance(node, list):
+        node_name = str(type(node))
+    stream.write(indent + first_i + node_name + "\n")
+    indent = indent + second_i
+    if hasattr(node, "children"):
+        children = list(node.children)
+    elif  isinstance(node, Definition):
+        children = list(node.assignment)
+    elif not isinstance(node, Iterable) or isinstance(node, str) or isinstance(node, tokenize.TokenInfo):
+        return
+    else:
+        children = list(node)
+    for i, child in enumerate(children):
+        _render_graph(
+            stream,
+            child,
+            indent=indent,
+            last=(i + 1) == len(children),
+            first=False,
+        )
 
 class Scope(NamedTuple):
     name: str
     options: ScopeOptions
     children: List[Union[Scope, Definition]]  # type: ignore
 
+    def print_scope(self):
+        stream = io.StringIO()
+        _render_graph(stream, self)
+        print(stream.getvalue())
+
+    def __str__(self):
+        return f"Scope {self.name}"
 
 class Definition(NamedTuple):
     name: str
     assignment: Any
     options: DefinitionOptions
 
-
-class ScopeDefinition(NamedTuple):
-    name: ScopeName
-    assignment: List = None
-    options: List = None
-    children: List = None
-
-    # def __init__(self, name, defaults, options, children):
+    def __str__(self):
+        return f"Definition {self.name} = {len(self.assignment)}"
 
 
 def combine_strings(parts):
