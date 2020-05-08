56bab104cb05abb58752b445651c2a9a604eeb8b
Nicholas Devenish <ndevenish@gmail.com>
2020-05-08 19:49:57 +0100

    include subscopes

--- tests/test_ossify.py
+++ tests/test_ossify.py
@@ -161,6 +161,8 @@ def test_nested_scope_definition():
 def test_include():
     parse_string("include file something.phil")
     parse_string("include scope a.b.c.d")
+    # subscope
+    parse_string("include scope a.b.c.d smething")
 
 
 def test_scope_inline_options():
