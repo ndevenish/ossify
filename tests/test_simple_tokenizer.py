767db0b81e4fb71e04c4ea156c9856973e3acbb5
Nicholas Devenish <ndevenish@gmail.com>
2020-05-08 18:44:18 +0100

    Always verbose for tokenizer test

--- tests/test_simple_tokenizer.py
+++ tests/test_simple_tokenizer.py
@@ -1,11 +1,12 @@
 import pytest
+
 from ossify.tokenizer import character_generator
 
 
 @pytest.mark.skip("is a test test")
 def test_character_generator():
     with open("/Users/nickd/dials/ossify/all_dials.phil") as f:
-        gen = character_generator(f)
+        gen = character_generator(f, verbose=True)
 
         for t in gen:
             pass
